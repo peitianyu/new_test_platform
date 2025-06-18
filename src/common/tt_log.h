22d536954e822a4ac1acf1ceda91f1b23ff122f6
tiantian <1216582461@qq.com>
2025-06-18 17:25:06 +0800

    opt: 优化log打印

--- src/common/tt_log.h
+++ src/common/tt_log.h
@@ -10,19 +10,24 @@ O & print_base(O &out, Args&&... args) {
     return out;
 } 
 
-#define TT_PRINT_RED       "\033[31m"
-#define TT_PRINT_YELLOW    "\033[33m"
-#define TT_PRINT_GREEN     "\033[32m"
-#define TT_PRINT_BLUE      "\033[34m"
-#define TT_PRINT_WHITE     "\033[37m"
-#define TT_PRINT_GRAY      "\033[90m"
-#define TT_PRINT_CYAN      "\033[36m"
-#define TT_PRINT_PURPLE    "\033[35m"
-#define TT_PRINT_NONE      "\033[0m"
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
+[[maybe_unused]]
+static std::string get_curr_time_str() {
+    auto now = std::chrono::system_clock::now();
+    auto now_time_t = std::chrono::system_clock::to_time_t(now);
+    auto now_tm = *std::localtime(&now_time_t);
+    auto now_us = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()) % 1000000;
+    std::ostringstream oss;
+    oss << "[" << std::put_time(&now_tm, "%Y-%m-%d %H-%M-%S") << ":" << std::setw(6) << std::setfill('0') << now_us.count() << "]";
+    return oss.str();
+} 
 
-#define LOG(...) print_base(std::cout, __VA_ARGS__, TT_PRINT_NONE) << std::endl;
-#define LOG_INFO(...) LOG(TT_PRINT_GREEN, __VA_ARGS__);
-#define LOG_WARN(...) LOG(TT_PRINT_YELLOW, __VA_ARGS__);
-#define LOG_ERROR(...) LOG(TT_PRINT_RED, __VA_ARGS__);
+#define LOG(...) print_base(std::cout, __VA_ARGS__) << std::endl;
+#define LOG_INFO(...) LOG(get_curr_time_str(), " [INFO] ", __VA_ARGS__);
+#define LOG_WARN(...) LOG(get_curr_time_str(), " [WARN] ", __VA_ARGS__);
+#define LOG_ERROR(...) LOG(get_curr_time_str(), " [ERRO] ", __VA_ARGS__);
 
 #endif // __COMMON_LOG_H__
